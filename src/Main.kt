44cdc38b639aca0a4ad2287694460f501f481767
sergioaguilar13y19 <sergioaguilar13y19@gmail.com>
2024-04-07 11:41:57 -0600

    fix problems

--- src/Main.kt
+++ src/Main.kt
@@ -16,7 +16,7 @@ class Bodega {
         }
         products.add(product)
         product.stock = cantidad
-        println("producto agregado ${product.name}, ${product.stock}")
+        println("producto agregado ${product.name}, ${product.stock} ${product.price}")
     }
 
     fun deleteProduct(product: Product, cantidad: Int) {
@@ -28,13 +28,6 @@ class Bodega {
         println("Stock actualizado = ${product.stock} ${product.name}")
     }
 
-    fun getProducts() {
-        products.forEach {
-            println(it)
-        }
-        println(this.products)
-       // return products
-    }
 }
 
 class Empleado(val DNI: Int, val nombre: String) {
@@ -44,6 +37,7 @@ class Empleado(val DNI: Int, val nombre: String) {
     var edad = 0
 
     var bodega = Bodega()
+    var vendidos = 0
     var dineroTotal = 0.0
     fun cargarStock(product: Product, cantidad: Int) {
         bodega.addProduct(product, cantidad)
@@ -54,18 +48,15 @@ class Empleado(val DNI: Int, val nombre: String) {
             println("producto insuficiente para ser vendido")
         } else {
             bodega.deleteProduct(product, cantidad)
+            dineroTotal = dineroTotal + (product.price * cantidad)
         }
-        dineroObtenido(product)
+       // dineroObtenido(product)
 
     }
 
     private fun dineroObtenido(product: Product) {
         val total = product.price * product.stock
         dineroTotal = total
-    }
-
-    fun products() {
-        bodega.getProducts()
     }
 
     fun corteCaja() {
@@ -83,42 +74,47 @@ fun main() {
     var negocio2 = Empleado(456, "Antonio")
     var negocio3 = Empleado(789, "Beatriz")
 
-    negocio1.cargarStock(television, 30)
-    negocio2.cargarStock(licuadora, 30)
-    negocio3.cargarStock(refrigerador, 30)
-
-    //dia 1
-    //negocio2.venderProducto(licuadora, 15)
-    //dia 2
-    negocio3.cargarStock(refrigerador, 10)
-    //dia 3
-    //negocio1.venderProducto(television,5)
-    //dia 4
-    //negocio1.cargarStock(television,1)
-    //dia 5
-    //negocio2.venderProducto(licuadora,10)
-    //dia 6
-    negocio3.venderProducto(refrigerador, 3)
-    //dia 7
-    //negocio2.cargarStock(licuadora,10)
-    //dia 8
-    //negocio1.cargarStock(refrigerador,5)
-    negocio3.venderProducto(refrigerador, 5)
-    //dia 9
-    //negocio2.cargarStock(television,7)
-    //negocio1.venderProducto(television,7)
-    //println("compro al negocio1")
-    //negocio2.cargarStock(refrigerador,7)
-    negocio3.venderProducto(refrigerador, 7)
-    //dia 10
-    //println("vende negocio1")
-    //negocio1.venderProducto(television,5)
-    println("vende negocio2")
-    //negocio2.venderProducto(licuadora,5)
-    println("vende negocio3")
-    negocio3.venderProducto(refrigerador, 5)
+    negocio1.cargarStock(television,10)
+    negocio1.venderProducto(television,2)
+    negocio1.cargarStock(television,2)
+    negocio1.venderProducto(television,1)
     negocio1.corteCaja()
-    negocio2.corteCaja()
-    negocio3.corteCaja()
-    negocio2.products()
+
+//    negocio1.cargarStock(television, 90)
+//    negocio2.cargarStock(licuadora, 90)
+//    negocio3.cargarStock(refrigerador, 90)
+//
+//    //dia 1
+//    negocio2.venderProducto(licuadora, 15)
+//    //dia 2
+//    negocio3.cargarStock(refrigerador, 10)
+//    //dia 3
+//    negocio1.venderProducto(television,5)
+//    //dia 4
+//    negocio1.cargarStock(television,1)
+//    //dia 5
+//    negocio2.venderProducto(licuadora,10)
+//    //dia 6
+//    negocio3.venderProducto(refrigerador, 10)
+//    //dia 7
+//    negocio2.cargarStock(licuadora,10)
+//    //dia 8
+//    negocio1.cargarStock(refrigerador,5)
+//    negocio3.venderProducto(refrigerador, 5)
+//    //dia 9
+//    negocio2.cargarStock(television,7)
+//    negocio1.venderProducto(television,7)
+//    //println("compro al negocio1")
+//    //negocio2.cargarStock(refrigerador,7)
+//    negocio3.venderProducto(refrigerador, 7)
+//    //dia 10
+//    //println("vende negocio1")
+//    //negocio1.venderProducto(television,5)
+//    println("vende negocio2")
+//    //negocio2.venderProducto(licuadora,5)
+//    println("vende negocio3")
+//    negocio3.venderProducto(refrigerador, 5)
+//    negocio1.corteCaja()
+//    negocio2.corteCaja()
+//    negocio3.corteCaja()
 }
